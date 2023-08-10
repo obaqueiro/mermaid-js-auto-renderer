c1fe7796798a64d4c24fde3d5a2dcc6160a10407
Omar Baqueiro Espinosa <obaqueiro@gmail.com>
2023-08-10 16:20:05 -0600

    Extract directives from text and initialize mermaid

--- mermaid-js-autorenderer.js
+++ mermaid-js-autorenderer.js
@@ -1,13 +1,35 @@
 // mermaid-diagrams auto-renderer; renders a document using JS-sequence-diagrams 
 // Omar Baqueiro (omar@baqueiro.com)
 // MIT License
-const diagram = document.body.innerHTML;
+document.body.removeChild(document.body.getElementsByTagName('script')[0]);
+
+const source = document.body.innerHTML;
+let settings = {};
 document.body.textContent = "";
+
+// Extract the settings and the diagram content
+const parts = source.split("%%");
+let diagram = null;
+
+if (parts.length > 1) {
+    settings = JSON.parse(parts[1].split(":").slice(1).join(':').trim().slice(0, -1));
+    diagram = parts[2];
+} else {
+    diagram = parts[0];
+}
+
+console.log(diagram)
+
 const x = document.createElement('pre');
 x.classList.add('mermaid');
 x.innerHTML= diagram;
-x.removeChild(x.getElementsByTagName('script')[0])
 document.body.appendChild(x);
 
-const a = document.createElement('script'); a.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/mermaid/10.3.0/mermaid.min.js');
+const a = document.createElement('script'); 
+a.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/mermaid/10.3.0/mermaid.min.js');
 document.head.appendChild(a);
+a.onload = function() {
+    // Initialize the mermaid library with the extracted settings
+    mermaid.initialize(settings);
+};
+
